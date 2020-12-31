f86e13e4240573cb0fc76d761fd7491d5b273d2c
manan228 <68659116+manan228@users.noreply.github.com>
2020-12-31 17:01:56 +0530

    final without design

---
+++ app.js
@@ -0,0 +1,22 @@
+//storing the data into variables to use it in js
+
+var input = document.querySelector("#input")
+var process = document.querySelector("#process")
+var output = document.querySelector("#output")
+
+// When click it will call the onClick function
+process.addEventListener("click", onClick)
+
+function onClick() {
+
+    var inputText = input.value;
+    
+    if (inputText%4 == 0){
+
+        output.innerHTML = "It is leap year"
+
+    } else {
+
+        output.innerHTML = "It is not a leap year"
+    }
+}
